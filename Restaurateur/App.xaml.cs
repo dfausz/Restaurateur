44dcf58779b0aa34b45c6734e470dca2f38eaf79
dfausz <dbfausz@gmail.com>
2023-10-28 21:46:36 -0500

    Initial push

---
+++ Restaurateur/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Restaurateur
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Restaurateur/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Restaurateur
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
