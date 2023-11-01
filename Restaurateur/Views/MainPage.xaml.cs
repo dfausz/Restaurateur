796c19f064873050a77afcc55c0641230f9d9c48
dfausz <dbfausz@gmail.com>
2023-10-31 23:01:32 -0500

    MVVM Example code

---
+++ Restaurateur/Views/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Restaurateur.ViewModels;
+
+namespace Restaurateur.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainViewModel();
+        }
+    }
+}
